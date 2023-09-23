14486ee5ba66aafa108bf733e3e2a0078ae99027
rrodrigodlima <rdrigo019@gmail.com>
2023-09-23 14:27:10 -0300

    created the endpoint GET /recipe/:name

--- src/recipes-api/Controllers/RecipesController.cs
+++ src/recipes-api/Controllers/RecipesController.cs
@@ -34,7 +34,9 @@ public class RecipesController : ControllerBase
     [HttpGet("{name}", Name = "GetRecipe")]
     public IActionResult Get(string name)
     {
-        throw new NotImplementedException();
+        Recipe recipe = this._service.GetRecipe(name);
+        if (recipe == null) return NotFound();
+        return Ok(recipe);
     }
 
     // 3 - Sua aplicação deve ter o endpoint POST /recipe
